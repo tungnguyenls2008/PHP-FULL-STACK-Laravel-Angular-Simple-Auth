d8b9c9eed57729b271d44d332f56481815a6c5de
OverLord <overlord@localhost.localdomain>
2020-06-26 16:48:18 +0700

    added todo controller

--- laravel-module/routes/web.php
+++ laravel-module/routes/web.php
@@ -36,3 +36,14 @@ Route::get('/auth', function () {
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('todos', 'TodosController@index');
+Route::get('todos/{todo}', 'TodosController@show');
+Route::get('new-todos', 'TodosController@create');
+Route::post('store-todos', 'TodosController@store');
+
+Route::get('todos/{todo}/edit', 'TodosController@edit');
+Route::post('todos/{todo}/update-todos', 'TodosController@update');
+
+Route::get('todos/{todo}/delete', 'TodosController@destroy');
+
+Route::get('todos/{todo}/complete', 'TodosController@complete');
